33d681924341416457212d0fd7e79ac84c0d0075
Manoel1622 <manoel_amancio@hotmail.com>
2022-01-06 22:55:52 -0300

    master

--- lib/features/covid_data/presentation/pages/info_page.dart
+++ lib/features/covid_data/presentation/pages/info_page.dart
@@ -5,18 +5,32 @@ class InfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Image.asset(
-            'images/info.png',
-            fit: BoxFit.cover,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Informações'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Image.network(
+                'https://image.flaticon.com/icons/png/512/63/63830.png',
+                width: 150,
+                height: 150,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text('App feito para estudos afins de conhecer as diversas, funções e metodos em Flutter'),
+              )
+            ],
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text('App feito para estudos afins de conhecer as diversas, funções e metodos em Flutter')
-        ],
+        ),
       ),
     );
   }
